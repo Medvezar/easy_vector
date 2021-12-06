8dbeab05a477544118dc84e1d3ad35dca0d2c831
Michael <77501487+Medvezar@users.noreply.github.com>
2021-12-06 12:42:45 +0300

    Add files via upload

--- file_01.cpp
+++ file_01.cpp
@@ -8,13 +8,20 @@ void itc_even_index_list(const vector <int> &mass,  vector <int> &mass1){
 }
 
 
+void itc_even_parts_list(const vector <int> &mass, vector <int> &mass1){
+    for (int n1 = 0; n1 < mass.size(); n1++)
+        if (mass[n1] % 2 == 0)
+            mass1.push_back(mass[n1]);
+}
+
+
 int main(){
-    vector <int> mass(5);
-    for (int n1 = 0; n1 < 5; n1++)
+    vector <int> mass(9);
+    for (int n1 = 0; n1 < 9; n1++)
         mass[n1] = n1 + 1;
     vector <int> mass1;
     // ---------------
-    itc_even_index_list(mass, mass1);
+    itc_even_parts_list(mass, mass1);
     //-------------------
     for (int n1 = 0; n1 < mass.size(); n1++)
         cout << mass[n1] << endl;
