4be7d8abc234bdf305ec67bb9366a5ccdc2127d0
Michael <77501487+Medvezar@users.noreply.github.com>
2021-12-06 12:33:39 +0300

    Add files via upload

---
+++ easy_list.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void itc_even_index_list(const vector <int> &mass,  vector <int> &mass1);
---
+++ file_01.cpp
@@ -0,0 +1,24 @@
+#include "easy_list.h"
+
+
+void itc_even_index_list(const vector <int> &mass,  vector <int> &mass1){
+    for (int n1 = 0; n1 < mass.size(); n1++)
+        if ((n1 + 1) % 2 == 0)
+            mass1.push_back(mass[n1]);
+}
+
+
+int main(){
+    vector <int> mass(5);
+    for (int n1 = 0; n1 < 5; n1++)
+        mass[n1] = n1 + 1;
+    vector <int> mass1;
+    // ---------------
+    itc_even_index_list(mass, mass1);
+    //-------------------
+    for (int n1 = 0; n1 < mass.size(); n1++)
+        cout << mass[n1] << endl;
+    cout << "------------------------------------" << endl;
+    for (int n1 = 0; n1 < mass1.size(); n1++)
+        cout << mass1[n1] << endl;
+}
