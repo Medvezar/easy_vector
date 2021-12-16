f08c08eb9aa6f96d31cea42e6ab82222a94919fe
Michael <77501487+Medvezar@users.noreply.github.com>
2021-12-16 12:44:23 +0300

    Add files via upload

--- file_01.cpp
+++ file_01.cpp
@@ -2,21 +2,25 @@
 
 
 void itc_even_index_list(const vector <int> &mass,  vector <int> &mass1){
-    for (int n1 = 0; n1 < mass.size(); n1++)
-        if ((n1 + 1) % 2 == 0)
-            mass1.push_back(mass[n1]);
+    if (mass.size() > 0)
+        for (int n1 = 0; n1 < mass.size(); n1++)
+            if ((n1 + 1) % 2 == 0)
+                mass1.push_back(mass[n1]);
 }
 
 
 void itc_even_parts_list(const vector <int> &mass, vector <int> &mass1){
-    for (int n1 = 0; n1 < mass.size(); n1++)
-        if (mass[n1] % 2 == 0)
-            mass1.push_back(mass[n1]);
+    if (mass.size() > 0)
+        for (int n1 = 0; n1 < mass.size(); n1++)
+            if (mass[n1] % 2 == 0)
+                mass1.push_back(mass[n1]);
 }
 
 
 int itc_positive_list(const vector <int> &mass) {
     int times = 0;
+    if (mass.size() == 0)
+        return 0;
     for (int n1 = 0; n1 < mass.size(); n1++)
         if (mass[n1] >= 0)
             times++;
@@ -26,6 +30,8 @@ int itc_positive_list(const vector <int> &mass) {
 
 int itc_sl_list(const vector <int> &mass) {
     int times = 0;
+    if (mass.size() == 0)
+        return 0;
     for (int n1 = 1; n1 < mass.size(); n1++)
         if (mass[n1 - 1] < mass[n1])
             times++;
@@ -34,8 +40,25 @@ int itc_sl_list(const vector <int> &mass) {
 
 
 bool itc_same_parts_list(const vector <int> &mass) {
+    if (mass.size() == 0)
+        return false;
     for (int n1 = 1; n1 < mass.size(); n1++)
         if ((mass[n1 - 1] < 0 and mass[n1] < 0) or (mass[n1 - 1] >= 0 and mass[n1] >= 0))
             return true;
     return false;
+}
+
+
+int main(){
+    vector <int> mass(0);
+    vector <int> mass1;
+    for (int n1 = 0; n1 < 0; n1++)
+        mass.push_back(n1 + 1);
+    cout << "-------------------" << endl;
+
+    for (int n1 = 0; n1 < mass1.size(); n1++)
+        cout << mass1[n1] << endl;
+    cout << "_____________________________" << endl;
+    for (int n1 = 0; n1 < mass.size(); n1++)
+        cout << mass[n1] << endl;
 }
--- file_02.cpp
+++ file_02.cpp
@@ -3,10 +3,12 @@
 
 void itc_rev_list(vector <int> &mass){
 	int numTest, len = mass.size();
-	for (int n1 = 0; n1 < len / 2; n1++) {
-		numTest = mass[len - n1 - 1];
-		mass[len - n1 - 1] = mass[n1];
-		mass[n1] = numTest;
+	if (len > 0){
+        for (int n1 = 0; n1 < len / 2; n1++) {
+            numTest = mass[len - n1 - 1];
+            mass[len - n1 - 1] = mass[n1];
+            mass[n1] = numTest;
+        }
 	}
 }
 
@@ -15,28 +17,34 @@ void itc_rev_par_list(vector <int>& mass) {
 	int numTest, len = mass.size();
 	if (len % 2 == 1)
 		len--;
-	for (int n1 = 1; n1 < len; n1++)
-		if ((n1 + 1) % 2 == 0){
-			numTest = mass[n1 - 1];
-			mass[n1 - 1] = mass[n1];
-			mass[n1] = numTest;
-		}
+    if (len > 0){
+        for (int n1 = 1; n1 < len; n1++)
+            if ((n1 + 1) % 2 == 0){
+                numTest = mass[n1 - 1];
+                mass[n1 - 1] = mass[n1];
+                mass[n1] = numTest;
+            }
+    }
 }
 
 
 void itc_rshift_list(vector <int>& mass) {
 	int numTest, len = mass.size();
 	vector <int> mass1(len);
-	for (int n1 = 0; n1 < len; n1++)
-		mass1[(n1 + 1) % len] = mass[n1];
-	mass = mass1;
+    if (len > 0){
+        for (int n1 = 0; n1 < len; n1++)
+            mass1[(n1 + 1) % len] = mass[n1];
+        mass = mass1;
+    }
 }
 
 
 void itc_super_shift_list(vector <int>& mass, int n) {
 	int numTest, len = mass.size();
 	vector <int> mass1(len);
-	for (int n1 = 0; n1 < len; n1++)
-		mass1[(n1 + n) % len] = mass[n1];
-	mass = mass1;
+	if (len > 0){
+        for (int n1 = 0; n1 < len; n1++)
+            mass1[(n1 + n) % len] = mass[n1];
+        mass = mass1;
+	}
 }
